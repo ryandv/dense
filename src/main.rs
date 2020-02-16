2d2ffce44f8777b9344c780dfb33cb7cdb1e747e
Ryan DV <rdevilla@uwaterloo.ca>
2020-02-16 17:16:29 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::UdpSocket;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let socket = UdpSocket::bind("8.8.8.8:53").await;
+    let query: &str = "\x00\x00\x01\x00\x01\x00\x00\x00\x00\x06google\x02ca\x01\x01";
+    socket.unwrap().send(query.as_bytes()).await
 }
